887280ccbd4a92c7bf755ad1a4b162acaeda6fca
Nick <n.drobyshevski@pm.me>
2022-09-29 22:01:09 +0200

    Change project organisation
    main.py renamed to Game.py
    game object initialisation is now in the main.py file

--- Card.py
+++ Card.py
@@ -1,19 +1,20 @@
-class Card():
-    # Ajoute ID 
-    def __init__(self, value:str, type:str) -> None:
+class Card:
+    # Ajoute ID
+    def __init__(self, value: str, type: str) -> None:
         self.type = type
         self.value = value
-    
+
     # def get_card(self):
     def __repr__(self) -> str:
-      return f"value: {self.value}, type:{self.type}"
-    
+        return f"value: {self.value}, type:{self.type}"
+
     def __str__(self) -> str:
-      return f"value: {self.value}, type:{self.type}"
-  
+        return f"value: {self.value}, type:{self.type}"
+
     def get_data(self):
-        return {"value": {self.value}, "type":{self.type}}
-    
-class  MetaCard(Card):
-    def __init__(self,value:str) -> None:
+        return {"value": {self.value}, "type": {self.type}}
+
+
+class MetaCard(Card):
+    def __init__(self, value: str) -> None:
         super().__init__(value=value, type="meta")
